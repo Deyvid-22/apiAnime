09c93ca8ec6307a543e7e9e2150a2e471086055d
Deyvid silva <silvadeyvid017@gmail.com>
2024-04-12 15:54:43 -0300

    finalizando projeto

--- src/app.js
+++ src/app.js
@@ -1,25 +1,41 @@
 import express, { query } from "express";
-import cors from "cors"
 const app = express()
 app.use(express.json())
-app.use(cors())
-
 
 const animes = ["dragon ball","one piece", "naruto", "hunter X hunter"]
+
+app.use((req,res,next)=>{
+     console.log("url chamada ", req.url)
+    return next()
+})
+
+function checkAnime(req, res, next){
+     if(!req.body.name){
+        return res.status(400).json({error:"nome do anime obrigatorio"})
+     }
+     return next()
+}
+
+function checkIdAnime(req,res,next){
+    
+     if(!animes[req.params.id]){
+          return res.status(400).json({error:"nome do anime não existe"})
+       }
+       return next()
+}
 
 app.get("/anime", (req,res)=>{
     return res.json(animes)
 })
 
 
-app.get("/anime/:id",(req,res)=>{
+app.get("/anime/:id",checkAnime, checkIdAnime,(req,res)=>{
      const {id} = req.params
 
      return res.json(animes[id])
-     console.log(nome)
 })
 
-app.post("/anime",(req,res)=>{
+app.post("/anime", checkAnime,(req,res)=>{
      const {name} = req.body
      animes.push(name)
 
@@ -27,25 +43,19 @@ app.post("/anime",(req,res)=>{
 })
 
 
-app.put("/anime/:id",(req,res)=>{
+app.put("/anime/:id",checkAnime ,checkIdAnime,(req,res)=>{
    const {id} = req.params
    const {name} = req.body
-
    animes[id] = name
 
    return res.json(animes)
-
 })
 
-app.delete("/anime/:id",(req,res)=>{
+app.delete("/anime/:id",checkIdAnime,checkIdAnime,(req,res)=>{
      const {id} = req.params
      animes.splice(id, 1)
 
      return res.json(animes)
-
 })
-
-
-
 
 export default app
